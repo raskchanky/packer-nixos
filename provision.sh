6e5695a19b63fe1cfa0921c7bab293ba37b6c310
Josh Black <raskchanky@gmail.com>
2014-01-03 21:04:06 -0800

    changing the provisioning

--- provision.sh
+++ provision.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 nix-env -iA nixos.pkgs.haskellPlatform
-nix-env -i ghc
+nix-env -iA nixos.pkgs.haskellPackages.cabalInstall_1_18_0_2
+nix-env -iA nixos.pkgs.vim
+nix-env -iA nixos.pkgs.apacheHttpd_2_4
